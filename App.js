935eb9b992cd18a1493ffaec715c9d43c3be07fc
Kwesi Manteaw <kosmanteaw@st.ug.edu.gh>
2024-05-27 12:24:45 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My name is <Text style = {styles.boldText}Kwesi></Text> Manteaw</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+text:{
+  fontSize:24,
+},
+boldText:{
+  fontWeight: 'bold',
+  fontSize: 24,
+}
+})
